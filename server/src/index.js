554d041eb87d4e38bb66ff599929fea6c251c947
Gabe Harms <gabeharms@gmail.com>
2019-05-05 18:21:15 -0700

    Adding server side

--- server/src/index.js
+++ server/src/index.js
@@ -37,29 +37,21 @@ const resolvers = {
         info,
       )
     },
-    createUser(parent, {name, ofAge}) {
-      return {
-        name,
-        ofAge,
-      }
-    },
   },
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: req => {
-    return {
-      ...req,
-      db: new Prisma({
-        typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
-        endpoint: 'https://us1.prisma.sh/public-viridianfin-527/my-wicked-blog/dev', // the endpoint of the Prisma API
-        debug: true, // log all GraphQL queries & mutations sent to the Prisma API
-        // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
-      }),
-    }
-  },
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
+      endpoint: 'https://us1.prisma.sh/public-viridianfin-527/my-wicked-blog/dev', // the endpoint of the Prisma API
+      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+      // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
+    }),
+  }),
 })
 
 server.start(() => console.log('Server is running on http://localhost:4000'))
